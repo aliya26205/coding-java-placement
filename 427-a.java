2e93424366cd530e971c7111137ff2351f407b82
Aliya <127426092+aliya26205@users.noreply.github.com>
2026-08-10 12:25:04 +0530

    Implement police event handling with user input

--- 427-a.java
+++ 427-a.java
@@ -21,3 +21,34 @@ class Main {
       System.out.println(untreated);
     }
 }
+
+//using arrays
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ente the n size:");
+        int n=sc.nextInt();
+        int untreated=0,availablepolice=0;
+        int[] event = new int[100];
+       
+        int count=n;
+        System.out.println("Enter n elements:");
+        for(int i=1;i<=n;i++){
+            event[i] = sc.nextInt();
+           
+            if(event[i]>0){
+                availablepolice+=event[i];
+            }
+            else{
+                if(availablepolice>0)
+                   availablepolice--;
+                else
+                  untreated++;
+            }
+        }
+      System.out.println(untreated);
+    }
+}
