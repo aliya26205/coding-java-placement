96aa74b77218e613b528c5276577701dbd9ab948
Aliya <127426092+aliya26205@users.noreply.github.com>
2026-08-13 14:37:17 +0530

    Implement frequency count of elements using hash maps
    Added a frequency counting method using hash maps.

---
+++ freq-of-ele.java
@@ -0,0 +1,50 @@
+//normAL method
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int maxCount=0;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count>maxCount){
+                maxCount=count;
+            }
+        }
+         System.out.println(maxCount);
+    }
+}
+
+//using hash maps
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+         
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if (map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }else{
+                map.put(arr[i],1);
+            }
+        }
+        System.out.println(map);
+    }
+}
