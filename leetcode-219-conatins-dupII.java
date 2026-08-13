7f16fd3c4d6938634d6cc4efc65d6c3eff705c7c
Aliya <127426092+aliya26205@users.noreply.github.com>
2026-08-13 10:49:25 +0530

    Improve comments in containsNearbyDuplicate method
    Refactor containsNearbyDuplicate method to clarify comments.

--- leetcode-219-conatins-dupII.java
+++ leetcode-219-conatins-dupII.java
@@ -14,13 +14,21 @@ class Solution {
 //using hashmaps timecomplexcity O(n)
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer,Integer>map=new HashMap<>();
+        Map<Integer,Integer>map=new HashMap<>();//create map 
          for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])&&i-map.get(nums[i])<=k)
+            if(map.containsKey(nums[i])&&i-map.get(nums[i])<=k)//do we have i in map eg i==3 num=1 we already have in map so 3-0 3<3 so return true
                return true;
-            map.put(nums[i],i);
+            map.put(nums[i],i);//adds values to empty map
          }
          return false;
         
     }
 }
+
+/*
+1 2 3 1
+0 1 2 3
+
+hashmap
+1:0 , 2:1, 3:2, 
+*/
