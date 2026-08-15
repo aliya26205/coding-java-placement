7530764657642784596044cccb2c18a4176d61e9
Aliya <127426092+aliya26205@users.noreply.github.com>
2026-08-15 09:46:05 +0530

    Document recursion concepts in recursion.java
    Add comments explaining recursion and its types.

---
+++ recursion.java
@@ -0,0 +1,15 @@
+/*
+Recursion:- function calling itself 
+types of recursion [6 types]
+1.direct
+2.indirect
+3.head[no tail]
+4.tail
+5.linear
+6.tree
+
+base case:
+1. adding 
+2. sustracting
+3. diving
+*/
