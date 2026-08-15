3481b910fa60deb91712676e9004f4987baa6d76
Aliya <127426092+aliya26205@users.noreply.github.com>
2026-08-15 15:24:30 +0530

    Implement Fibonacci using dynamic programming
    Added dynamic programming method for Fibonacci calculation.

--- fibbonaci.java
+++ fibbonaci.java
@@ -1,4 +1,4 @@
-
+// with normal recursion
 import java.util.*;
 class Main {
     public static int fibo(int n){
@@ -14,5 +14,20 @@ class Main {
         int n=sc.nextInt();
 
         System.out.println(fibo(n));
+    }
+}
+// with dynamic program method(dp)(memoizing)
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        long dp[]=new long[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.println(dp[n]);
     }
 }
