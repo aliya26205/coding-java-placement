b2f577cc5ccdc8dd9d713a934aec1d7253a72753
Aliya <127426092+aliya26205@users.noreply.github.com>
2026-08-15 09:53:22 +0530

    Add recursive function to print countdown
    Implemented a recursive function to print numbers from 5 to 1.

--- recursion.java
+++ recursion.java
@@ -12,4 +12,22 @@ base case:
 1. adding 
 2. sustracting
 3. diving
+
+recursion has call stack
 */
+
+// program 1 :- to print 5 4 3 2 1 with recursion
+class Main {
+    public static void fun(int n){
+        if(n<=0)
+          return;
+        else{
+          System.out.print(n+" ");
+          fun(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        fun(5);
+    }
+}
