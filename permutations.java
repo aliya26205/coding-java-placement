4fc82c2730edadb65fc360a769f6f7ca5b03dfc9
Aliya <127426092+aliya26205@users.noreply.github.com>
2026-08-15 10:40:40 +0530

    Enhance permutation output to show all combinations
    Updated output to include all permutations of the input string.

--- permutations.java
+++ permutations.java
@@ -6,5 +6,27 @@ b.pick one char ,
 c.fix it at the current position ,
 d.recursion permutation remaining characters 
 input: str="abc"
-output abc acb
+output abc acb bac bca cab cba
 */
+
+
+import java.util.*;
+class Main {
+    public static void permu(String prefix,String remain){
+        if(remain.length()==0){
+          System.out.println(prefix);
+          return;
+        }
+        for(int i=0;i<remain.length();i++){
+            char ch=remain.charAt(i);
+            String nextRemain=remain.substring(0,i)+remain.substring(i+1);
+            permu(prefix+ch,nextRemain);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int n=s.length();
+        permu("",s);
+    }
+}
