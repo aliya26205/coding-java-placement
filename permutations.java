4dff572b664d8e49edd0fe6b74d2a29b363f7b1b
Aliya <127426092+aliya26205@users.noreply.github.com>
2026-08-15 09:33:21 +0530

    Document permutations function with explanationk
    Add a comment explaining the permutations function and its approach.

---
+++ permutations.java
@@ -0,0 +1,10 @@
+/*
+1. permutations(string):- given a string print all permutatoions of the string using recursion
+Approach 1:- 
+a.fix one character permute the rest ,
+b.pick one char , 
+c.fix it at the current position ,
+d.recursion permutation remaining characters 
+input: str="abc"
+output abc acb
+*/
