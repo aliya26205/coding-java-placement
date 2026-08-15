f7972620638b716a9b55a939830e3069c5d997d6
Aliya <127426092+aliya26205@users.noreply.github.com>
2026-08-15 10:19:24 +0530

    Add recursive method to print name in reverse

--- recursion.java
+++ recursion.java
@@ -71,3 +71,23 @@ class Main {
         printName(s,n-1);
     }
 }
+
+// program 4: print name in reverse order
+import java.util.*;
+class Main {
+    public static void printName(String name,int n){
+        if(n<0)
+          return;
+        else{
+            char ch=name.charAt(n);
+            System.out.println(ch);
+            printName(name,n-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int n=s.length();
+        printName(s,n-1);
+    }
+}
